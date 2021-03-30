8abea60c02fa550f2c77c5920fcffe4da7401d8a
lguevara <lrosarioguevara20@gmail.com>
2021-03-29 21:55:35 -0600

    Se agrega archivo template.py

---
+++ template/template.py
@@ -0,0 +1,24 @@
+'''NAME
+       template.py
+
+VERSION
+        1.0
+
+AUTHOR
+	Luz Rosario Guevara Cruz
+
+DESCRIPTION
+        
+
+CATEGORY
+        Genomic sequence
+
+USAGE
+
+    
+
+    example
+
+    
+
+'''
