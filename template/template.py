92e6f2e426883f8f9d57d90be369b9490ad9f8a0
HeladiaSalgado <heladia@ccg.unam.mx>
2021-04-09 13:09:04 -0500

    Template Revision
    Agregando algunas etiquetas y complementando la descripción de las ya existentes

--- template/template.py
+++ template/template.py
@@ -1,24 +1,40 @@
-'''NAME
-       template.py
+'''
+NAME
+       [programName].py
 
 VERSION
-        1.0
+        [#.#]
 
 AUTHOR
-	Luz Rosario Guevara Cruz
+	Luz Rosario Guevara Cruz <[username]@gmail.com>
+	[Other authors]: [Modifications]
 
 DESCRIPTION
-        
+        [Describe the program]
 
 CATEGORY
-        Genomic sequence
+        [category of the program: sequence analysis, genomic sequence for example]
+
 
 USAGE
-
+	[programName][-options/arguments]
     
 
-    example
-
+ARGUMENTS
+    [name]  [description]
+    [name]  [description]
+    [name]  [description]
     
+INPUT
+    [files or directories used to run the program and formats]
+    
+    
+OUTPUT
+    [file names and formats]
+    
+    
+EXAMPLES
+    [Example 1: describe the example, input and outputs]
+
 
 '''
